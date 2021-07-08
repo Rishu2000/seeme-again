2da4de8be465fb3748a90d9676143edf9f8940da
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-08 10:44:51 +0530

    Backend server boiler-plate ready.

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(server, {
+    cors:{
+        origin: '*',
+        method: ['GET','POST'],
+    }
+})
+
+app.use(cors());
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/',(req, res) =>{
+    res.json("Server is running");
+})
+
+server.listen(PORT,() => {
+    console.log("Server listening on port "+PORT)
+})
