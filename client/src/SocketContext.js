9e3188958dae83418b5a5cd72e5fade06c4b4835
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-08 16:05:48 +0530

    All the methods part are done, just need to provide this conponent.

--- client/src/SocketContext.js
+++ client/src/SocketContext.js
@@ -9,8 +9,15 @@ const socket = io('http://localhost:5000');
 const ContextProvider = ({children}) => {
 
     const [stream, setStream] = useState();
+    const [me, setMe] = useState('');
+    const [call, setCall] = useState({});
+    const [callAccepted, setCallAccepted] = useState(false);
+    const [callEnded, setCallEnded] = useState(false);
+    const [name, setName] = useState('')
 
     const myVideo = useRef();
+    const userVideo = useRef();
+    const connectionRef = useRef();
 
     useEffect(() => {
         navigator.mediaDevices.getUserMedia({video: true,audio: true})
@@ -18,15 +25,43 @@ const ContextProvider = ({children}) => {
                 setStream(currentStream);
                 myVideo.current.srcObject = currentStream;
             })
+            stream.on('me',(id) => {
+                setMe(id);
+            })
+            stream.on('calluser',({from,name:callerName,signal}) => {
+                setCall({isReceivedCall:true,from, name:callerName, signal})
+            })
     }, [])
-
-    const callUser = () => {
-
-    }
+    
     const answerCall = () => {
-
+        setCallAccepted(true);
+        const peer = new Peer({initiator:false, trickle:false, stream});
+        peer.on('signal',(data) => {
+            socket.emit('answercall', {signal:data, to: call.from});
+        })
+        peer.on('stream', (currentStream) => {
+            userVideo.current.srcObject = currentStream;
+        })
+        peer.signal(call.signal);
+        connectionRef.current = peer;
+    }
+    const callUser = (id) => {
+        const peer = new Peer({initiator:false, trickle:false, stream});
+        peer.on('signal',(data) => {
+            socket.emit('calluser',{userToCall:id, signalData:data, from:me, name })
+        })
+        peer.on('stream',(currentStream) =>{
+            userVideo.current.srcObject = currentStream
+        })
+        socket.on('callaccepted',(signal) => {
+            setCallAccepted(true);
+            peer.signal(signal);
+        })
+        connectionRef.current = peer;
     }
     const leaveCall = () => {
-
+        setCallEnded(true);
+        connectionRef.current.destroy();
+        window.location.reload();
     }
 }
