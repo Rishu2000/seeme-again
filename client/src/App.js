23fc025cf40722d19e2bc13d9e4caeedbd8e6012
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-08 12:53:08 +0530

    Structure for front-end has been created.

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,47 @@
 import React from 'react'
+import {Typography, AppBar} from '@material-ui/core'
+import Notifications from "./components/Notifications"
+import Options from "./components/Options"
+import VideoPlayer from "./components/VideoPlayer"
+import { makeStyles } from '@material-ui/core/styles'
+
+const useStyles = makeStyles((theme) => ({
+  appBar: {
+    borderRadius: 15,
+    margin: '30px 100px',
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    width: '600px',
+    border: '2px solid black',
+
+    [theme.breakpoints.down('xs')]: {
+      width: '90%',
+    },
+  },
+  image: {
+    marginLeft: '15px',
+  },
+  wrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+  },
+}))
 
 const App = () => {
+  const classes = useStyles();
   return (
-    <div>
-      App
+    <div className={classes.wrapper}>
+      <AppBar className={classes.appBar} position="static" color="inherit">
+        <Typography variant="h2" align="center">Video Chat</Typography>
+      </AppBar>
+      <VideoPlayer/>
+      <Options>
+        <Notifications/>
+      </Options>
     </div>
   )
 }
