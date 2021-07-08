c79efe1f09faf6cb2e3f872bfbda089cc5366531
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-08 17:20:00 +0530

    While using Context of React we have to use Provider like this.

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import "./Styles.css";
+import { ContextProvider } from './SocketContext';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextProvider>
+      <App />
+    </ContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
