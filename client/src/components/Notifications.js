38c9f47253f51f25db1ed6c162d696c82da1e7f7
Rishav Kumar Shah <kumarrishavshah2000@gmail.com>
2021-07-10 15:02:45 +0530

    Notification Component complete.

--- client/src/components/Notifications.js
+++ client/src/components/Notifications.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, {useContext, useEffect} from 'react'
+import {Button} from '@material-ui/core'
+import {SocketContext} from "../SocketContext"
 
 const Notifications = () => {
+    const {call, callAccepted, answerCall} = useContext(SocketContext);
+    // useEffect(() => {
+    //     console.log(call);
+    // }, [])
+    // const buttonClicked = () => {
+    //     console.log(call);
+    //     answerCall();
+    // }
     return (
-        <div>
-            Notifications
-        </div>
+        <>
+            {call.isReceivedCall && !callAccepted && (
+                <div style={{display:"flex", justifyContent:"center"}}>
+                    <h1>{call.name} is calling:  </h1>
+                    <Button
+                        variant="contained"
+                        color="primary"
+                        onClick={answerCall}
+                    >
+                        Answer
+                    </Button>
+                </div>
+            )}
+        </>
     )
 }
 
